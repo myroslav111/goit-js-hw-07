25b465805b5f1b82ffd7789b7649cb0ada0b12c9
myroslav <myrkozar@gmail.com>
2022-04-12 16:33:13 +0300

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// ссылка на родителя
+const refsGalleryContainer = document.querySelector('.gallery')
+
+// готовая разметка
+const gallerySet = createGallery(galleryItems)
+
+//фун. разметки галереи
+function createGallery(arr) {
+const newArr = arr.map(el => `<a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}"  />
+</a>`).join('')
+
+    return newArr
+}
+
+// вставка разметки в html
+refsGalleryContainer.innerHTML = gallerySet
+
+// инициализация библиотеки 
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', createOptionTextAndDelay) 
+
+// фун. задержки заголовка
+// иниц.заголовка
+function createOptionTextAndDelay() {
+    gallery.defaultOptions.captionsData = 'alt'
+    gallery.defaultOptions.captionDelay = 250
+}
